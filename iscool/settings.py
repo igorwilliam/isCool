9adf5d79e0ca3cf4e9df28b06d7a696ebdebd381
Igor William <igorwm9@gmail.com>
2018-01-20 15:51:37 -0300

    added messages in settings

--- iscool/settings.py
+++ iscool/settings.py
@@ -132,3 +132,13 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_URL = 'logout'
 AUTH_USER_MODEL = 'accounts.User'
+
+#messages
+from django.contrib.messages import constants as messages_constants
+MESSAGE_TAGS = {
+    messages_constants.DEBUG: 'debug',
+    messages_constants.INFO: 'info',
+    messages_constants.SUCCESS: 'success',
+    messages_constants.WARNING: 'warning',
+    messages_constants.ERROR: 'danger',
+}
