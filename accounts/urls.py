4234f2df685449136df83f1d3488c809970b9a50
Igor William <igorwm9@gmail.com>
2018-01-20 15:07:42 -0300

    added change info and password user

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('novo-usuario/', views.addUser, name='addUser'),
     path('minha-conta/', views.myAccount, name='myAccount'),
+    path('atualizar-dados/', views.updateUser, name='updateUser'),
+    path('atualizar-senha/', views.updatePassword, name='updatePassword'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import PasswordChangeForm
 
 from .models import User
 from .forms import UserAdminCreationForm
@@ -18,5 +19,34 @@ class MyAccountView(LoginRequiredMixin, TemplateView):
 
     template_name = "accounts/minha-conta.html"
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+
+    model = User
+    template_name = 'accounts/update-user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:myAccount')
+
+
+    def get_object(self):
+        return self.request.user
+
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+
+    template_name = 'accounts/update-password.html'
+    success_url = reverse_lazy('accounts:myAccount')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(UpdatePasswordView, self).form_valid(form)
+
+
+updateUser = UpdateUserView.as_view()
 addUser = RegisterView.as_view()
 myAccount = MyAccountView.as_view()
+updatePassword = UpdatePasswordView.as_view()
