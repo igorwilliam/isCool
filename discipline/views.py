0582b5f9862fc41e134c152e6397dded9148cc85
Igor William <igorwm9@gmail.com>
2018-01-23 12:17:00 -0300

    fixed warning post

--- discipline/views.py
+++ discipline/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from discipline.models import Discipline, RegisterStudent
-
 from discipline.forms import DisciplineForm, RegisterStudentForm
 
 # Create your views here.
